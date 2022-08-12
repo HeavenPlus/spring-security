6be2bab74a497fdea421f3f1ea438b9625f1a72c
zhanggq <zhanggq@lingjia.tech>
2022-08-12 17:15:42 +0800

    init

--- springSecurity/src/main/java/com/heaven/springsecurity/config/SecurityConfiguration.java
+++ springSecurity/src/main/java/com/heaven/springsecurity/config/SecurityConfiguration.java
@@ -1,6 +1,9 @@
 package com.heaven.springsecurity.config;
 
+import com.heaven.springsecurity.service.impl.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -13,10 +16,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         super.configure(auth);
+        //数据库认证
+        auth.userDetailsService(myUserDetailsService);
     }
+
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -65,5 +75,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 //    @Bean
 //    public WebSecurityCustomizer webSecurityCustomizer(){
 //        return (web) -> web.ignoring().antMatchers("/css/**", "/images/**", "/js/**", "/favicon.ico");
+//    }
+
+//    public AuthenticationManager authenticationManager(){
+//
 //    }
 }
--- springSecurity/src/main/java/com/heaven/springsecurity/service/impl/MyUserDetailsService.java
+++ springSecurity/src/main/java/com/heaven/springsecurity/service/impl/MyUserDetailsService.java
@@ -1,18 +1,41 @@
 package com.heaven.springsecurity.service.impl;
 
+import com.heaven.springsecurity.domain.User;
+import com.heaven.springsecurity.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * @author zhanggq
  * @date 2022/8/9 13:42
  */
+@Service
 public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-        return null;
+        User user = userService.findByUsername(username);
+        if(null == user){
+            throw new UsernameNotFoundException(username);
+        }
+        Collection<? extends GrantedAuthority> authorities = new ArrayList<>();
+        UserDetails userDetails = new org.springframework.security.core.userdetails.User(user.getUsername(),
+                "{noop}" + user.getPassword(), //{noop} 不加密
+                true, //用户是否启用
+                true, //用户是否过期 true未过期
+                true,// 用户凭据是否过期 true未过期
+                true,//用户是否锁定 true未锁定
+                authorities);//权限集合
+        return userDetails;
     }
 }
